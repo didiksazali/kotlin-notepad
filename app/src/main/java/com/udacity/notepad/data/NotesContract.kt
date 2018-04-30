fe622942aa43a1d31ec5592e4cdbbf5fe26581d8
omrobbie <putu.roby@yahoo.com>
2018-04-30 08:09:59 +0800

    agar lebih rapi, ubah dengan string template

--- app/src/main/java/com/udacity/notepad/data/NotesContract.kt
+++ app/src/main/java/com/udacity/notepad/data/NotesContract.kt
@@ -3,6 +3,10 @@ package com.udacity.notepad.data
 import android.provider.BaseColumns
 
 import com.udacity.notepad.data.NotesContract.NoteTable.CREATED_AT
+import com.udacity.notepad.data.NotesContract.NoteTable.IS_PINNED
+import com.udacity.notepad.data.NotesContract.NoteTable.TEXT
+import com.udacity.notepad.data.NotesContract.NoteTable.UPDATED_AT
+import com.udacity.notepad.data.NotesContract.NoteTable._ID
 import com.udacity.notepad.data.NotesContract.NoteTable._TABLE_NAME
 
 object NotesContract {
@@ -15,13 +19,13 @@ object NotesContract {
         val UPDATED_AT = "updated_at"
     }
 
-    val SQL_CREATE_ENTRIES = "CREATE TABLE " + _TABLE_NAME + " (" +
-            NoteTable._ID + " INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, " +
-            NoteTable.TEXT + " TEXT, " +
-            NoteTable.IS_PINNED + " INTEGER, " +
-            CREATED_AT + " INTEGER, " +
-            NoteTable.UPDATED_AT + " INTEGER" +
-            ")"
+    val SQL_CREATE_ENTRIES = """CREATE TABLE $_TABLE_NAME (
+        $_ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+        $TEXT TEXT,
+        $IS_PINNED INTEGER,
+        $CREATED_AT INTEGER,
+        $UPDATED_AT INTEGER
+    )""".trimMargin()
 
     val SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS $_TABLE_NAME"
 
